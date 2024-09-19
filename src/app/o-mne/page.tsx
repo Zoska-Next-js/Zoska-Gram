f1e57b7f67d4697e2430dd69775f0b950a285b3d
LukasLubusky <lukas02.lubusky@gmail.com>
2024-09-19 08:32:33 +0200

    Metadata added

---
+++ src/app/o-mne/page.tsx
@@ -0,0 +1,19 @@
+// src/app/o-mne/page.tsx
+
+
+
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+export const metadata = { title:"O mne | ZoskaSnap" };
+
+
+export default function About() {
+  return (
+    <Container>
+      <Typography>
+        O nás
+      </Typography>
+    </Container>
+  );
+}
---
+++ src/app/podmienky/page.tsx
@@ -0,0 +1,19 @@
+// src/app/podmienky/page.tsx
+
+
+
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+export const metadata = { title:"Podmienky | ZoskaSnap" };
+
+
+export default function TermsConditions() {
+  return (
+    <Box>
+      <Typography>
+        Podmienky používania
+      </Typography>
+    </Box>
+  );
+}
